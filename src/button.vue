b5649fecde8b4d33e0a2030b29cbdc6923de687e
gelory <917589697@qq.com>
2018-09-01 20:58:39 +0800

    添加icon

--- src/button.vue
+++ src/button.vue
@@ -1,12 +1,13 @@
 <template>
-    <button class="g-button">
+    <button class="g-button" :class="`icon-${iconPosition}`">
+        <svg v-if="icon" class="icon"><use :xlink:href="`#i-${icon}`"></use></svg>
         <slot></slot>
     </button>
 
 </template>
 <script>
     export default {
-
+        props:['icon','iconPosition']
     }
 </script>
 <style>
@@ -17,6 +18,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background-color: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
     }
     .g-button:hover{
         border-color: var(--border-active-color);
@@ -26,5 +31,16 @@
     }
     .g-button:focus{
         outline: none;
+    }
+    .icon{
+        width:1em;
+        height:1em;
+        margin-left: 0;
+        margin-right: .3em;
+    }
+    .icon-right .icon{
+        order:2;
+        margin-left: .3em;
+        margin-right: 0;
     }
 </style>
